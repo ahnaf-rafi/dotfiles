f12924da6b8e55e7df5a2192cf31c4c9debbd443
Ahnaf Rafi <ahnaf.al.rafi@gmail.com>
2026-08-07 23:45:37 -0400

    Add hyprland-session.target and kanshi.service.

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -4,6 +4,7 @@
 -----------------
 
 hl.on("hyprland.start", function ()
+  hl.exec_cmd("systemctl --user start hyprland-session.target")
   hl.exec_cmd("nm-applet")
   hl.exec_cmd("blueman-applet")
   hl.exec_cmd("kanshi")
